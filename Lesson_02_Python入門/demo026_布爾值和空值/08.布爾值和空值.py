48ce68a2b024cd16e4270207724557effc59ecd4
eason <regretyou710@gmail.com>
2022-04-17 13:33:40 +0800

    202204171333

---
+++ Lesson_02_Python入門/demo026_布爾值和空值/08.布爾值和空值.py
@@ -0,0 +1,15 @@
+# 布爾值（bool）
+# 布爾值主要用來做邏輯判斷
+# 布爾值一共有兩個 True 和 False
+# True表示真 False表示假
+a = True
+a = False
+# print('a =',a)
+
+# 布爾值實際上也屬於整型，True就相當於1，False就相當於0
+# print(1 + False)
+
+# None（空值）
+# None專門用來表示不存在
+b = None
+print(b)
